ed5e415c4b4ba506e5ced8c0b2c948d30f6e6621
Julien Lamon <J.A.D.Lamon@student.tudelft.nl>
2020-03-06 15:07:14 +0100

    getNextMove changes. Still have to resolve dead ends

--- Assignment2/Swarm Intelligence/src/Ant.java
+++ Assignment2/Swarm Intelligence/src/Ant.java
@@ -54,30 +54,66 @@ public class Ant {
     public Direction getNextMove(ArrayList<Coordinate> visited) {
         SurroundingPheromone surroundingPheromone = maze.getSurroundingPheromone(currentPosition);
 
-        HashMap<Double, Direction> directions = new HashMap<>();
+        HashMap<Double, ArrayList<Direction>> directions = new HashMap<>();
 
         double north = surroundingPheromone.get(Direction.North);
         double probaNorth = north/(surroundingPheromone.getTotalSurroundingPheromone());
         if(north!=0 && !visited.contains(currentPosition.add(Direction.dirToCoordinateDelta(Direction.North)))) {
-            directions.put(probaNorth, Direction.North);
+            if(!directions.containsKey(probaNorth)) {
+                ArrayList<Direction> toAdd = new ArrayList<>();
+                toAdd.add(Direction.North);
+                directions.put(probaNorth, toAdd);
+            }
+            else {
+                ArrayList<Direction> previous = directions.get(probaNorth);
+                previous.add(Direction.North);
+                directions.put(probaNorth, previous);
+            }
         }
 
         double south = surroundingPheromone.get(Direction.South);
         double probaSouth = south/(surroundingPheromone.getTotalSurroundingPheromone());
         if(south!=0 && !visited.contains(currentPosition.add(Direction.dirToCoordinateDelta(Direction.South)))) {
-            directions.put(probaSouth, Direction.South);
+            if(!directions.containsKey(probaSouth)) {
+                ArrayList<Direction> toAdd = new ArrayList<>();
+                toAdd.add(Direction.South);
+                directions.put(probaSouth, toAdd);
+            }
+            else {
+                ArrayList<Direction> previous = directions.get(probaSouth);
+                previous.add(Direction.South);
+                directions.put(probaSouth, previous);
+            }
         }
 
         double east = surroundingPheromone.get(Direction.East);
         double probaEast = east/(surroundingPheromone.getTotalSurroundingPheromone());
         if (east!=0 && !visited.contains(currentPosition.add(Direction.dirToCoordinateDelta(Direction.East)))) {
-            directions.put(probaEast, Direction.East);
+            if(!directions.containsKey(probaEast)) {
+                ArrayList<Direction> toAdd = new ArrayList<>();
+                toAdd.add(Direction.East);
+                directions.put(probaEast, toAdd);
+            }
+            else {
+                ArrayList<Direction> previous = directions.get(probaEast);
+                previous.add(Direction.East);
+                directions.put(probaEast, previous);
+            }
         }
 
         double west = surroundingPheromone.get(Direction.West);
         double probaWest = west/(surroundingPheromone.getTotalSurroundingPheromone());
         if(west!= 0 && !visited.contains(currentPosition.add(Direction.dirToCoordinateDelta(Direction.West)))) {
-            directions.put(probaWest, Direction.West);
+            if(!directions.containsKey(probaWest)) {
+                ArrayList<Direction> toAdd = new ArrayList<>();
+                toAdd.add(Direction.West);
+                directions.put(probaWest, toAdd);
+            }
+            else {
+                ArrayList<Direction> previous = directions.get(probaWest);
+                previous.add(Direction.West);
+                directions.put(probaWest, previous);
+            }
         }
 
         double max = Integer.MIN_VALUE;
@@ -85,7 +121,10 @@ public class Ant {
             max = Math.max(max, proba);
         }
 
-        return directions.get(max);
+        ArrayList<Direction> list = directions.get(max);
+
+        if(list == null) return Direction.South;
+        else return list.get(rand.nextInt(list.size()));
     }
 }
 
