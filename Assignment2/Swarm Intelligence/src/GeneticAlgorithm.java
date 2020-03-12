699e12a6b0d1eed141f84df7d208dc8bcb94cd21
JulianBiesheuvel <j.p.biesheuvel@student.tudelft.nl>
2020-03-12 10:07:35 +0100

    temp

--- Assignment2/Swarm Intelligence/src/GeneticAlgorithm.java
+++ Assignment2/Swarm Intelligence/src/GeneticAlgorithm.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 public class GeneticAlgorithm {
     private int generations;
     private int popSize;
-    private static final int STOPPING = 10;
+    //private static final int STOPPING = 10;
     private int currentGenCount;
 
     /**
@@ -47,130 +47,37 @@ public class GeneticAlgorithm {
      * @return the optimized product sequence.
      */
     public int[] solveTSP(TSPData pd) {
-        int[] initPopulation = pd.getDistances()[0];
-        int[][] totalPopulation = new int[popSize][initPopulation.length];
-        double[] fitness = new double[popSize];
+        // 1) Create the initial generation with shuffle
 
-        // create starting population
-        for(int i = 0; i < popSize; i++) {
-            shuffle(initPopulation);
-            totalPopulation[i] = initPopulation;
+        int[][] distances = pd.getDistances();
+        int[][] initGeneration = new int[popSize][distances.length];
+        int[] initChild = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+
+        initGeneration[0] = initChild;
+        // Fill the initGeneration with random children
+        for(int i = 1; i < popSize; i++) {
+            int[] temp = Arrays.copyOf(initGeneration[i - 1], distances.length);
+            shuffle(temp);
+            initGeneration[i] = temp;
         }
 
-        int[][] newGeneration = new int[popSize][initPopulation.length];
-        int[] bestOfGen;
-        // while loop or something else
-        int counter = 0;
-        while (counter < generations) {
-            // 4 Compute the fitness for each child
-            for (int j = 0; j < totalPopulation.length; j++) {
-                fitness[j] = getFitness(totalPopulation[j], pd);
-            }
+        // 2) Determine the fitness of this generation
+        int[][]
+        // 3) Select the best (children with the highest fitness) children -> selection
+        // 4) Do crossover and mutation on these children
+        // 5) Put the children back into the initial generation
+        // 6) Repeat step 1 - 5 until max number of generations is reached
 
-            // 5 Selection
-            int[] childA = selection(fitness, pd.getDistances());
-            int[] childB = selection(fitness, pd.getDistances());
-            // 6a Crossover
-            int[] crossover = crossOver(childA, childB);
-            // 6b Mutation
-            int[] mutation = mutation(crossover);
-            // 7 Put into new population
-            newGeneration[((int) (Math.random() * popSize - 1)) + 1] = mutation;
-            // 8 Replace the old population with the new population
-            totalPopulation = newGeneration;
-            counter++;
-        }
-
-        bestOfGen = selection(fitness, totalPopulation);
-
-        return bestOfGen;
+        return null;
     }
 
-    /**
-     * Select the offspring with the highest probabilities, the ones that are not selected are cloned
-     * @param fitness the fitness for one generation
-     * @param population current population
-     * @return the children with highest probability
-     */
-    private int[] selection(double[] fitness, int[][] population) {
-        int index = 0;
-
-        double r = Math.random();
-
-        while (r > 0) {
-            r = r - fitness[index];
-            index++;
-        }
-        index--;
-
-        return population[index];
-    }
-
-    private double getFitness(int[] child, TSPData pd) {
-        double sum = 0.0;
-
-        for (int value : child) {
-            sum += value;
-        }
-
-        double start = findDistance(child, pd.getStartDistances());
-        double end = findDistance(child, pd.getEndDistances());
-
-        sum = sum - start - end;
-
-        return (1/sum) * 1000;
-    }
-
-    private int findDistance(int[] child, int[] start) {
-        int dist = 0;
-
-        for (int value : child) {
-            for (int i : start) {
-                if (value == i) {
-                    dist = value;
-                    break;
-                }
-            }
-        }
-
-        return dist;
-    }
-
-    private int[] mutation(int[] child) {
-        // double pm = 0.001; <-- Ask this
-        // Bit flip or swapping two products?
-        int index1 = (int)(Math.random() * child.length);
-        int index2 = (int)(Math.random() * child.length);
-
-        int temp = child[index2];
-        child[index2] = child[index1];
-        child[index1] = temp;
-
-        return child;
-    }
-
-    private int[] crossOver(int[] child1, int[] child2) {
-        /// double crossoverProb = 0.7; <-- Ask this
-        int start = (int)(Math.random() * child1.length);
-        int end = (int)(Math.random() * child1.length) + 1 + start;
-        int[] newOffspringArray = Arrays.copyOfRange(child1, start, end);
-        List<Integer> newOffspring = Arrays.stream(newOffspringArray).boxed().collect(Collectors.toList());
-
-        for (int temp : child2) {
-            if (!newOffspring.contains(temp)) {
-                newOffspring.add(temp);
-            }
-        }
-
-        return newOffspring.stream().mapToInt(i -> (int) i).toArray();
-    }
 
     /**
      * Assignment 2.b
      */
     public static void main(String[] args) throws IOException, ClassNotFoundException {
     	//parameters
-    	int populationSize = 18;
+    	int populationSize = 20;
         int generations = 20;
         String persistFile = "./productMatrixDist";
         
