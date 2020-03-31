40008107bccaf736a88c08da47ef52241eb94e42
mkeeler <m.l.keeler@student.tudelft.nl>
2020-03-31 23:16:04 +0200

    Fixed bug, ran with varying neurons, created graph for 11

--- Assignment3/Neural_Networks/Perceptron/main.py
+++ Assignment3/Neural_Networks/Perceptron/main.py
@@ -1,26 +1,26 @@
 import perceptron as p
 import numpy as np
-import cross_validation as cv
+from Assignment3.Neural_Networks.Perceptron.cross_validation import *
 
 
 def main():
     features_file = np.genfromtxt('../features.txt', delimiter=',')
     targets_file = np.genfromtxt('../targets.txt', delimiter='')
 
-    param_features = cv.get_validation(features_file)
-    param_targets = cv.get_validation(targets_file)
+    param_features = get_validation(features_file)
+    param_targets = get_validation(targets_file)
 
-    cv_features = cv.create_validation(features_file)
-    cv_targets = cv.create_validation(targets_file)
+    cv_features = create_validation(features_file)
+    cv_targets = create_validation(targets_file)
 
     for i in range(10):
-        test_set_length = cv.get_length(cv_targets)
+        test_set_length = get_length(cv_targets)
 
-        features_testing = cv.create_test(file=cv_features, l=test_set_length, i=i)
-        targets_testing = cv.create_test(file=cv_targets, l=test_set_length, i=i)
+        features_testing = create_test(file=cv_features, l=test_set_length, i=i)
+        targets_testing = create_test(file=cv_targets, l=test_set_length, i=i)
 
-        features_training = cv.create_training(file=cv_features, l=test_set_length, i=i)
-        targets_training = cv.create_training(file=cv_targets, l=test_set_length, i=i)
+        features_training = create_training(file=cv_features, l=test_set_length, i=i)
+        targets_training = create_training(file=cv_targets, l=test_set_length, i=i)
 
         neurons = 30
 
