0aa42abc1250b25a99860bc0ff5d97eef19d2945
JulianBiesheuvel <j.p.biesheuvel@student.tudelft.nl>
2020-04-02 15:23:55 +0200

    fixed main

--- Assignment3/Neural_Networks/Perceptron/main.py
+++ Assignment3/Neural_Networks/Perceptron/main.py
@@ -7,26 +7,35 @@ def main():
     features_file = np.genfromtxt('../features.txt', delimiter=',')
     targets_file = np.genfromtxt('../targets.txt', delimiter='')
 
-    param_features = cv.get_validation(features_file)
-    param_targets = cv.get_validation(targets_file)
+    # however this is what Liudas first initiated, so something like 75 - 15 - 10?
+    # but we have to write this in the report - in the code we are using the ..._testing as validation
+    # these comments needed to be deleted afterwards
+    features_training, features_validation, features_testing = \
+        features_file[:5890], features_file[5890:7068], features_file[7068:]
+    targets_training, targets_validation, targets_testing = \
+        targets_file[:5890], targets_file[5890:7068], targets_file[7068:]
 
-    cv_features = cv.create_validation(features_file)
-    cv_targets = cv.create_validation(targets_file)
-
-    for i in range(10):
-        test_set_length = cv.get_length(cv_targets)
-
-        features_testing = cv.create_test(file=cv_features, l=test_set_length, i=i)
-        targets_testing = cv.create_test(file=cv_targets, l=test_set_length, i=i)
-
-        features_training = cv.create_training(file=cv_features, l=test_set_length, i=i)
-        targets_training = cv.create_training(file=cv_targets, l=test_set_length, i=i)
-
-        neurons = 30
-
-        perceptron = p.Perceptron(features_training=features_training, targets_training=targets_training, neurons=neurons)
-        perceptron.train()
-        perceptron.test(features_test=features_testing, targets_test=targets_testing)
+    # NEEDED FOR K_FOLD CROSS VALIDATION
+    # param_features = get_validation(features_file)
+    # param_targets = get_validation(targets_file)
+    #
+    # cv_features = create_validation(features_file)
+    # cv_targets = create_validation(targets_file)
+    #
+    # for i in range(1):
+    #     test_set_length = get_length(cv_targets)
+    #
+    #     features_testing = create_test(file=cv_features, l=test_set_length, i=i)
+    #     targets_testing = create_test(file=cv_targets, l=test_set_length, i=i)
+    #
+    #     features_training = create_training(file=cv_features, l=test_set_length, i=i)
+    #     targets_training = create_training(file=cv_targets, l=test_set_length, i=i)
+    #
+    #     neurons = 30
+    #
+    #     perceptron = p.Perceptron(features_training=features_training, targets_training=targets_training, neurons=neurons)
+    #     perceptron.train()
+    #     perceptron.test(features_test=features_testing, targets_test=targets_testing)
 
 
 if __name__ == "__main__":
